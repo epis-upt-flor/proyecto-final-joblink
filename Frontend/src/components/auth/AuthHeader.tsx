ee21f0121e85ab769e06731fbe0542f223ca960f
xo2021071080 <xo2021071080@virtual.upt.pe>
2025-03-31 00:08:54 +0000

    [@ximena-ortiz] Logo UPT Vista Login

--- Frontend/src/components/auth/AuthHeader.tsx
+++ Frontend/src/components/auth/AuthHeader.tsx
@@ -1,18 +1,22 @@
-import { GraduationCap } from "lucide-react";
+import { GraduationCap } from "lucide-react"
+import uptLogo from "@/assets/UPT.png"
 
 export default function AuthHeader() {
   return (
     <div className="w-full p-6 flex flex-col lg:flex-row items-start lg:items-center justify-between border-b border-border">
-      {/* Logo + Nombre */}
+      {/* LinkJob */}
       <div className="flex items-center gap-2">
         <GraduationCap className="h-6 w-6 text-primary" />
         <span className="font-bold text-xl text-foreground">LinkJob</span>
       </div>
 
-      {/* Universidad */}
-      <div className="font-bold text-xl text-foreground">
-        Universidad Privada de Tacna
+      {/* UPT */}
+      <div className="flex items-center gap-2">
+      <img src={uptLogo} alt="UPT Logo" className="h-10 w-auto" />
+        <span className="font-bold text-xl text-foreground whitespace-nowrap">
+          Universidad Privada de Tacna
+        </span>
       </div>
     </div>
-  );
+  )
 }
