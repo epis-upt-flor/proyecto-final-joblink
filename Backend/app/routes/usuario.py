90a51196b8868b19dd31c50239ed9608d598f568
mangoesafterplay <ja2021069823@virtual.upt.pe>
2025-04-09 10:19:35 -0500

    [@mangoesafterplay] Limpieza de ruta Usuario

--- Backend/app/routes/usuario.py
+++ Backend/app/routes/usuario.py
@@ -10,24 +10,4 @@ router = APIRouter()
 def listar_usuarios(db: Session = Depends(get_db)):
     return listar_usuarios(db)
 
-@router.post("/usuarios/")
-async def registrar_usuario_endpoint(request: Request, db: Session = Depends(get_db)):
-    data = await request.json()  # Recibimos todo el cuerpo de la solicitud como un diccionario
-    
-    # Asegurarse de que los datos necesarios estén presentes
-    if not data.get("email") or not data.get("password") or not data.get("rol") or not data.get("username"):
-        raise HTTPException(status_code=400, detail="Faltan datos obligatorios")
 
-    # Llamar a la función `registrar_usuario` pasando los valores de `data`
-    usuario = registrar_usuario(
-        data=data, 
-        db=db, 
-        username=data.get("username"),
-        email=data.get("email"),
-        password=data.get("password"),
-        rol=data.get("rol")
-    )  # Pasamos los valores individuales de data
-    return {
-        "message": "Usuario registrado con éxito",
-        "usuario_id": usuario.id
-    }
