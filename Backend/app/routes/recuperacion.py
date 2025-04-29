fd625191b61f1281d38f4990f80b8b7331a907e9
mangoesafterplay <ja2021069823@virtual.upt.pe>
2025-04-28 22:50:58 -0500

    [@mangoesafterplay] Route recuperacion adaptado para la clase recuperacion_service

--- Backend/app/routes/recuperacion.py
+++ Backend/app/routes/recuperacion.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
-from app.models.database import get_db
 from pydantic import BaseModel
-from app.services.recuperacion_service import generar_token_y_enviar, cambiar_contrasena_con_token
+from app.models.database import get_db
+from app.services.recuperacion_service import RecuperacionService
 
-router = APIRouter(prefix="/recuperacion")
+router = APIRouter(prefix="/recuperacion", tags=["Recuperacion"])
+
+recuperacion_service = RecuperacionService()
+
 class EmailRequest(BaseModel):
     email: str
 
 @router.post("/solicitar/")
 async def solicitar_token(request: Request, db: Session = Depends(get_db)):
     data = await request.json()
-    print(data)
-    return generar_token_y_enviar(data["email"], db)
+    return recuperacion_service.generar_token_y_enviar(data["email"], db)
 
 @router.post("/cambiar/")
 async def cambiar_password(request: Request, db: Session = Depends(get_db)):
     data = await request.json()
-    return cambiar_contrasena_con_token(data["token"], data["nueva_contrasena"], db)
+    return recuperacion_service.cambiar_contrasena_con_token(data["token"], data["nueva_contrasena"], db)
