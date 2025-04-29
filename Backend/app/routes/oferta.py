85be1dc755261c176282f32ae7d33f19c3bd37ca
mangoesafterplay <ja2021069823@virtual.upt.pe>
2025-04-28 22:22:50 -0500

    [@mangoesafterplay] Route oferta adaptado para la clase oferta_service

--- Backend/app/routes/oferta.py
+++ Backend/app/routes/oferta.py
@@ -1,53 +1,43 @@
 from fastapi import APIRouter, Depends, Request, HTTPException
 from sqlalchemy.orm import Session
 from app.models.database import get_db
-from app.services.oferta_service import (
-    registrar_oferta,
-    listar_ofertas,
-    obtener_oferta_por_id,
-    actualizar_oferta,
-    eliminar_oferta
-)
+from app.services.oferta_service import OfertaService
 
 router = APIRouter(prefix="/ofertas", tags=["Ofertas"])
-
+oferta_service = OfertaService()
 
 @router.post("/")
 async def registrar_oferta_endpoint(request: Request, db: Session = Depends(get_db)):
     data = await request.json()
-    oferta = registrar_oferta(db, data)
+    oferta = oferta_service.registrar_oferta(db, data)
     if not oferta:
         raise HTTPException(
             status_code=400, detail="Error al registrar la oferta.")
     return {"message": "Oferta registrada", "id": oferta.id}
 
-
 @router.get("/")
 async def listar_ofertas_endpoint(db: Session = Depends(get_db)):
-    return listar_ofertas(db)
-
+    return oferta_service.listar_ofertas(db)
 
 @router.get("/{id}")
 async def obtener_oferta_endpoint(id: int, db: Session = Depends(get_db)):
-    oferta = obtener_oferta_por_id(db, id)
+    oferta = oferta_service.obtener_oferta_por_id(db, id)
     if not oferta:
         raise HTTPException(status_code=404, detail="Oferta no encontrada")
     return oferta
 
-
 @router.put("/{id}")
 async def actualizar_oferta_endpoint(id: int, request: Request, db: Session = Depends(get_db)):
     data = await request.json()
-    actualizada = actualizar_oferta(db, id, data)
+    actualizada = oferta_service.actualizar_oferta(db, id, data)
     if not actualizada:
         raise HTTPException(
             status_code=404, detail="Oferta no encontrada o error al actualizar")
     return {"message": "Oferta actualizada", "id": actualizada.id}
 
-
 @router.delete("/{id}")
 async def eliminar_oferta_endpoint(id: int, db: Session = Depends(get_db)):
-    ok = eliminar_oferta(db, id)
+    ok = oferta_service.eliminar_oferta(db, id)
     if not ok:
         raise HTTPException(status_code=404, detail="Oferta no encontrada")
     return {"message": "Oferta eliminada"}
