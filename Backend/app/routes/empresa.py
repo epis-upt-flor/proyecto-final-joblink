9449810650db775ba5328db3b55ef56438a7b4cf
mangoesafterplay <ja2021069823@virtual.upt.pe>
2025-05-08 10:53:55 -0500

    [@mangoesafterplay] Route de empresa adaptado para la clase empresa_service

--- Backend/app/routes/empresa.py
+++ Backend/app/routes/empresa.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from app.models.database import get_db
-from app.services.empresa_service import *
+from app.services.empresa_service import EmpresaService
 
-router = APIRouter()
+router = APIRouter(prefix="/empresas", tags=["Empresas"])
+empresa_service = EmpresaService()
 
-@router.get("/empresas/")
+@router.get("/")
 def listar_empresas_endpoint(db: Session = Depends(get_db)):
-    empresas = listar_empresas(db)
+    empresas = empresa_service.listar_empresas(db)
     if not empresas:
         raise HTTPException(status_code=404, detail="No hay empresas registradas.")
     return empresas
 
-@router.delete("/empresas/{empresa_id}")
+@router.delete("/{empresa_id}")
 def eliminar_empresa_endpoint(empresa_id: int, db: Session = Depends(get_db)):
-    return eliminar_empresa(db, empresa_id)
+    return empresa_service.eliminar_empresa(db, empresa_id)
 
-@router.put("/empresas/{empresa_id}")
+@router.put("/{empresa_id}")
 async def editar_empresa_endpoint(empresa_id: int, request: Request, db: Session = Depends(get_db)):
     data = await request.json()
     nombre = data.get("nombre")
@@ -24,7 +25,7 @@ async def editar_empresa_endpoint(empresa_id: int, request: Request, db: Session
     telefono = data.get("telefono")
     logo = data.get("logo")
 
-    empresa = editar_empresa(db, empresa_id, nombre, ruc, telefono, logo)
+    empresa = empresa_service.editar_empresa(db, empresa_id, nombre, ruc, telefono, logo)
     return {
         "message": "Empresa actualizada correctamente.",
         "empresa_id": empresa.id
